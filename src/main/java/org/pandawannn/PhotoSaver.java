cbb24769e1a5e763b1e911cc9283dcf805a27b2b
Thomas Neff <t.neff86@gmail.com>
2022-07-27 16:09:37 +0200

    support mp4 files

--- src/main/java/org/pandawannn/PhotoSaver.java
+++ src/main/java/org/pandawannn/PhotoSaver.java
@@ -9,16 +9,22 @@ import java.nio.file.attribute.BasicFileAttributeView;
 import java.nio.file.attribute.FileTime;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PhotoSaver {
 
     public static void reSaveFiles(File xmpFile, LocalDateTime date) {
         String pathWithoutExtension = FilenameUtils.removeExtension(xmpFile.getAbsolutePath());
-        resetTimes(new File(pathWithoutExtension + ".jpg"), date);
-        File livePhoto = new File(pathWithoutExtension + ".mov");
-        if (livePhoto.exists()) {
-            resetTimes(livePhoto, date);
-        }
+        findMediaFile(pathWithoutExtension).forEach(file -> resetTimes(file, date));
+    }
+
+    private static List<File> findMediaFile(String pathWithoutExtension) {
+        return Stream.of(".jpg", ".jpeg", ".mp4", ".mov")
+                .filter(extension -> new File(pathWithoutExtension + extension).exists())
+                .map(ext -> new File(pathWithoutExtension + ext))
+                .collect(Collectors.toList());
     }
 
     private static void resetTimes(File file, LocalDateTime date) {
